dc361feef6e8922bc1300627c8f0cceac5884e97
Theo-Morin <contact@theomorin.fr>
2019-07-21 16:02:30 +0200

    Opti index.js

--- client/index.php
+++ client/index.php
@@ -29,58 +29,11 @@ for($i = 0; $i < count($array); $i++)
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 	<script type="text/javascript" src="./public/assets/vendor/maphilight/maphilight.js"></script>
+	<script type="text/javascript" src="./public/assets/js/index.js"></script>
     <script type="text/javascript">
     $(function() {
 		$('.map').maphilight();
     });
-    </script>
-    <script>
-        function allowDrop(ev) {
-            ev.preventDefault();
-		}
-
-		function drag(ev) {
-            ev.dataTransfer.setData("text", ev.target.id);
-		}
-
-		function drop(ev) {
-			ev.preventDefault();
-			var target = ev.target;
-            var data = ev.dataTransfer.getData("text");
-            $(target).find(".contain").append(document.getElementById(data));
-        }
-        function onclick_page(event)
-        {
-            var x = event.clientX;
-            var y = event.clientY;
-            $(".container").append("<div id=\"centre\">");
-            var element = document.getElementById("centre");
-            var rect = element.getBoundingClientRect();
-            var y2 = y;
-            var x2 = x - rect.left;
-            var y3 = y2 + 5;
-            var x3 = x2;
-            $(".container").append("<div onclick=\"delete_point('" + y + "" + x + "')\" class=\"punaise\" id=\"" + y + "" + x + "\" style=\"margin-top:" + y2 + "px;margin-left:" + x2 + "px;\"></div>");
-            if($("#addtexte").val() != "")
-                $(".container").append("<div class=\"textepuce\" style=\"margin-top:" + y3 + "px;margin-left:" + x3 + "px;\" id=\"t" + y + "" + x + "\">" + $("#addtexte").val() + "</div>");
-            //alert('Vous avez cliqué au point de coordonnés: ' + x + ', ' + y );
-            $("#centre").remove();
-            console.log($("#addtexte").val());
-            $("#addtexte").val("");
-            //console.log(rect.bottom);
-        }
-        function delete_point(ev)
-        {
-            $("#" + ev).remove();
-            $("#t" + ev).remove();
-        }
-        function switch_zone(map)
-        {
-            if($("svg").length == 0)
-                document.location = './?map=true';
-            else
-                document.location = './?map=false';
-        }
     </script>
 </head>
 <body <?php if($nomap){ echo "onload='$(\"svg\").remove()'"; } ?> ondblclick="onclick_page(event)">
---
+++ client/public/assets/js/index.js
@@ -0,0 +1,46 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var target = ev.target;
+    var data = ev.dataTransfer.getData("text");
+    $(target).find(".contain").append(document.getElementById(data));
+}
+function onclick_page(event)
+{
+    var x = event.clientX;
+    var y = event.clientY;
+    $(".container").append("<div id=\"centre\">");
+    var element = document.getElementById("centre");
+    var rect = element.getBoundingClientRect();
+    var y2 = y;
+    var x2 = x - rect.left;
+    var y3 = y2 + 5;
+    var x3 = x2;
+    $(".container").append("<div onclick=\"delete_point('" + y + "" + x + "')\" class=\"punaise\" id=\"" + y + "" + x + "\" style=\"margin-top:" + y2 + "px;margin-left:" + x2 + "px;\"></div>");
+    if($("#addtexte").val() != "")
+        $(".container").append("<div class=\"textepuce\" style=\"margin-top:" + y3 + "px;margin-left:" + x3 + "px;\" id=\"t" + y + "" + x + "\">" + $("#addtexte").val() + "</div>");
+    //alert('Vous avez cliqué au point de coordonnés: ' + x + ', ' + y );
+    $("#centre").remove();
+    console.log($("#addtexte").val());
+    $("#addtexte").val("");
+    //console.log(rect.bottom);
+}
+function delete_point(ev)
+{
+    $("#" + ev).remove();
+    $("#t" + ev).remove();
+}
+function switch_zone(map)
+{
+    if($("svg").length == 0)
+        document.location = './?map=true';
+    else
+        document.location = './?map=false';
+}
