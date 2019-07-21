1d270725754074c08ae1ff32c798fc833317dc85
Theo-Morin <contact@theomorin.fr>
2019-07-21 19:25:00 +0200

    Zone sud-ouest

--- client/components/views/home.php
+++ client/components/views/home.php
@@ -30,7 +30,7 @@
         </svg>
         <div class="contain"></div>
     </div>
-    <div id="est" class="empty" ondrop="drop(event)" ondragover="allowDrop(event)" style="padding:15px;height:240px;width:220px;position:absolute; top:0; left:50%; transform:translate(-50%,0%); margin-top:260px;margin-left:113px;">
+    <div id="est" class="empty" ondrop="drop(event)" ondragover="allowDrop(event)" style="padding:15px;height:220px;width:220px;position:absolute; top:0; left:50%; transform:translate(-50%,0%); margin-top:260px;margin-left:113px;z-index:2;">
         <svg height="250" width="220">
             <polygon points="20,91,27,86,35,75,50,74,69,74,87,72,107,68,120,55,135,52,144,56,150,61,164,60,175,48,181,36,198,16,211,13,213,27,215,48,214,60,210,78,201,96,194,113,191,122,187,126,187,140,191,154,188,162,185,187,182,211,179,224,174,237,166,245,151,231,142,220,130,218,121,229,117,242,108,225,105,207,100,197,86,190,76,184,65,176,54,167,43,166,27,152,22,138,20,121,17,106" style="fill:black;stroke:#0080FF;stroke-width:3" />
         </svg>
@@ -42,12 +42,12 @@
         </svg>
         <div class="contain"></div>
     </div>
-    <!--<div id="sud-est" class="empty" ondrop="drop(event)" ondragover="allowDrop(event)" style="padding:15px;height:200px;width:351px;position:absolute; top:0; left:50%; transform:translate(-50%,0%); margin-top:105px;margin-left:44px;">
-        <svg height="210" width="351">
-            <polygon points="0,165,27,138,45,122,51,112,61,111,74,103,84,87,90,74,96,56,106,39,116,29,136,16,148,4,161,8,170,15,183,21,191,24,214,27,237,25,255,25,269,33,279,40,287,53,304,61,309,78,317,96,320,110,322,117,333,126,342,141,350,156,350,165,328,178,318,193,311,182,315,167,307,152,287,150,266,158,240,167,221,171,204,172,198,167,198,180,182,184,174,180,166,172,158,168,148,173,133,179,126,182,123,193,122,202,104,206,85,200,69,190,58,183,47,183,47,192,37,193,37,187,5,175" style="fill:black;stroke:#0080FF;stroke-width:3" />
+    <div id="sud-est" class="empty" ondrop="drop(event)" ondragover="allowDrop(event)" style="padding:15px;height:300px;width:220px;position:absolute; top:0; left:50%; transform:translate(-50%,0%); margin-top:365px;margin-left:95px;z-index:1;">
+        <svg height="260" width="210">
+            <polygon points="36,2,31,10,25,31,19,43,16,54,14,66,11,84,11,97,10,114,8,134,8,151,7,166,5,191,1,201,20,204,37,209,67,219,86,228,99,241,110,247,126,251,150,251,171,252,185,250,197,241,202,224,206,206,206,191,206,175,199,160,191,147,185,138,167,122,160,115,153,114,142,119,137,128,137,137,129,129,129,123,126,114,123,104,120,95,114,91,98,81,83,73,72,61,64,62,57,57,45,44,42,31,40,14" style="fill:black;stroke:#0080FF;stroke-width:3" />
         </svg>
         <div class="contain"></div>
-    </div>-->
+    </div>
     <div id="sud-ouest" class="empty" ondrop="drop(event)" ondragover="allowDrop(event)" style="padding:15px;height:250px;width:240px;position:absolute; top:0; left:50%; transform:translate(-50%,0%); margin-top:365px;margin-left:-95px;">
         <svg height="250" width="240">
             <polygon points="29,63,22,68,13,77,7,91,8,105,9,118,6,130,1,145,0,158,5,172,10,187,6,204,11,214,25,224,37,227,50,238,63,243,69,246,77,235,89,240,99,246,108,241,112,235,121,229,123,222,124,212,134,206,156,207,168,207,181,205,194,202,202,203,204,192,206,175,209,157,208,139,211,110,212,89,213,63,222,34,230,12,235,3,216,13,201,20,184,22,162,28,159,33,140,40,135,46,122,51,106,50,92,55,78,58,62,63,52,63,41,60" style="fill:black;stroke:#0080FF;stroke-width:3" />
